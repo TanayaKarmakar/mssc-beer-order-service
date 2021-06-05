2a0a0b95ac5520356d9ec5e384ecd13cc11e3126
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-06-05 18:06:19 +0530

    added validate action

---
+++ src/main/java/com/app/beer/order/service/statemachine/actions/ValidateOrderAction.java
@@ -0,0 +1,40 @@
+package com.app.beer.order.service.statemachine.actions;
+
+import com.app.beer.order.service.config.JmsConfig;
+import com.app.beer.order.service.domain.BeerOrder;
+import com.app.beer.order.service.domain.BeerOrderEventEnum;
+import com.app.beer.order.service.domain.BeerOrderStatusEnum;
+import com.app.beer.order.service.repositories.BeerOrderRepository;
+import com.app.beer.order.service.services.BeerOrderServiceImpl;
+import com.app.beer.order.service.web.mappers.BeerOrderMapper;
+import com.app.common.events.ValidateOrderRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * @author t0k02w6 on 05/06/21
+ * @project mssc-beer-order-service
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+    private final JmsTemplate jmsTemplate;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> stateContext) {
+        String beerOrderId = (String)stateContext.getMessage().getHeaders().get(BeerOrderServiceImpl.BEER_ORDER_HEADER);
+        BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE, ValidateOrderRequest.builder()
+        .beerOrderDto(beerOrderMapper.beerOrderToDto(beerOrder)));
+        log.debug("Sent validation order request to queue for order id : {}", beerOrderId);
+    }
+}
---
+++ src/main/java/com/app/common/events/ValidateOrderRequest.java
@@ -0,0 +1,19 @@
+package com.app.common.events;
+
+import com.app.common.model.BeerOrderDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author t0k02w6 on 05/06/21
+ * @project mssc-beer-order-service
+ */
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class ValidateOrderRequest {
+    private BeerOrderDto beerOrderDto;
+}
