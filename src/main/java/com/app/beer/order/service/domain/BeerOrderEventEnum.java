13c422c02c553a064d6846013060b4fec1b8c74a
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-06-05 08:20:25 +0530

    refactor order states

--- src/main/java/com/app/beer/order/service/domain/BeerOrderEventEnum.java
+++ src/main/java/com/app/beer/order/service/domain/BeerOrderEventEnum.java
@@ -5,7 +5,7 @@ package com.app.beer.order.service.domain;
  * @project mssc-beer-order-service
  */
 public enum BeerOrderEventEnum {
-    VALIDATE_ORDER, VALIDATION_PASSED, VALIDATION_FAILED,
+    VALIDATE_ORDER, VALIDATION_PASSED, VALIDATION_FAILED, ALLOCATE_ORDER,
     ALLOCATION_SUCCESS, ALLOCATION_NO_INVENTORY, ALLOCATION_FAILED,
     BEERORDER_PICKED_UP;
 }
--- src/main/java/com/app/beer/order/service/services/BeerOrderStateChangeInterceptor.java
+++ src/main/java/com/app/beer/order/service/services/BeerOrderStateChangeInterceptor.java
@@ -5,6 +5,7 @@ import com.app.beer.order.service.domain.BeerOrderEventEnum;
 import com.app.beer.order.service.domain.BeerOrderStatusEnum;
 import com.app.beer.order.service.repositories.BeerOrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.state.State;
@@ -18,19 +19,21 @@ import java.util.UUID;
  * @author t0k02w6 on 05/06/21
  * @project mssc-beer-order-service
  */
+@Slf4j
 @RequiredArgsConstructor
 public class BeerOrderStateChangeInterceptor extends StateMachineInterceptorAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
     private final BeerOrderRepository beerOrderRepository;
 
     @Override
     public void preStateChange(State<BeerOrderStatusEnum, BeerOrderEventEnum> state, Message<BeerOrderEventEnum> message, Transition<BeerOrderStatusEnum, BeerOrderEventEnum> transition, StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachine) {
-        Optional.ofNullable(message).ifPresent(msg -> {
-            Optional.ofNullable(UUID.class.cast(msg.getHeaders().getOrDefault(BeerOrderServiceImpl.BEER_ORDER_HEADER, UUID.fromString(""))))
-                    .ifPresent(id -> {
-                        BeerOrder beerOrder = beerOrderRepository.getOne(id);
-                        beerOrder.setOrderStatus(state.getId());
-                        beerOrderRepository.save(beerOrder);
-                    });
-        });
+        Optional.ofNullable(message)
+                .flatMap(msg -> Optional.ofNullable((String)msg.getHeaders()
+                        .getOrDefault(BeerOrderServiceImpl.BEER_ORDER_HEADER, "")))
+                .ifPresent(orderId -> {
+                    log.debug("Saving state for order id: " + orderId + " Status: " + state.getId());
+                    BeerOrder beerOrder = beerOrderRepository.getOne(UUID.fromString(orderId));
+                    beerOrder.setOrderStatus(state.getId());
+                    beerOrderRepository.saveAndFlush(beerOrder);
+                });
     }
 }
